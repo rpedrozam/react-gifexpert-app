bb935627dfcdfc7314e71fc67e84ed585292a0f6
rpedrozam <43505609+rpedrozam@users.noreply.github.com>
2020-09-20 21:41:21 -0500

    Docs Added

---
+++ docs/precache-manifest.d8a15346afcfc17f32e46d3ed5f363b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c185e1066eec78187b92ddda80f3b88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfc84fae3f2cff419148",
+    "url": "/static/css/main.aaf6498b.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc84fae3f2cff419148",
+    "url": "/static/js/main.0e6afab3.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
